98715a42f827d6d0f1bd6b031292ca228b2d9811
himuraaaaa <149753124+himuraaaaa@users.noreply.github.com>
2025-04-05 13:15:31 +0700

    Update About.jsx
    revisi

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
 
         <div className='about-content grid'>
           <div className='about-text'>
-            <h2 className='about-title fs-26 ls-1'>About BookHub</h2>
+            <h2 className='about-title fs-26 ls-1'>About BookFinder</h2>
             <p className='fs-17'>Selamat datang di BookFinder, mitra terpercaya Anda dalam menemukan buku terbaik untuk setiap kesempatan. Kami percaya bahwa buku memiliki kekuatan untuk mengubah hidup, memicu kreativitas, dan membuka pintu menuju dunia baru.
 
 Di BookFinder, kami mempermudah pencarian buku dengan menawarkan alat intuitif untuk menjelajahi genre, penulis, dan topik yang Anda minati. Baik Anda seorang pembaca santai, pecinta buku sejati, atau seseorang yang mencari referensi akademik, platform kami dirancang dengan kebutuhan Anda dalam pikiran.
